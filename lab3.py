e061a6c43d8a263b718d0eacd1ee7a176811ec57
kolyan3445 <55030059+kolyan3445@users.noreply.github.com>
2023-08-29 14:46:24 +0400

    Add files via upload

--- lab3.py
+++ lab3.py
@@ -1,100 +1,165 @@
-#Формируется матрица F следующим образом: если в В количество чисел, меньших К в нечетных столбцах в области 3 больше,
-# чем сумма чисел в четных строках в области 2, то поменять в Е симметрично области 3 и 2 местами,
-# иначе В и Е поменять местами несимметрично.
-# При этом матрица А не меняется. После чего вычисляется выражение: ((К*A)*F– K*AT .
-# Выводятся по мере формирования А, F и все матричные операции последовательно.
-
-# B C    2
-# E D  1   3
-#        4
-
+# Формируется матрица F следующим образом: если в В количество чисел,
+# меньших К в нечетных столбцах в области 3 больше, чем сумма чисел
+# в четных строках в области 2, то поменять в Е симметрично области
+# 3 и 2 местами, иначе В и Е поменять местами несимметрично.
+# При этом матрица А не меняется. После чего вычисляется выражение: (К*A)*F– K*AT .
 import random
 
 
-def print_matrix(matrix):
-    for row in matrix:
+def printm(mat):
+    for row in mat:
         for elem in row:
-            print('{:4}'.format(elem), end=' ')
+            print('{:5}'.format(elem), end=' ')
         print()
 
 
-def swap_symmetric(matrix, area1, area2):
-    n = len(matrix)
-    for i in range(n // 2):
-        for j in range(n // 2):
-            matrix[area1 + i][area1 + j], matrix[area2 + i][area2 + j] = matrix[area2 + i][area2 + j], matrix[area1 + i][area1 + j]
+def paste(matF, matrix, column_index, row_index):
+    a = column_index
+    for row in matrix:
+        for element in row:
+            matF[row_index][column_index] = element
+            column_index += 1
+        row_index += 1
+        column_index = a
 
 
-def swap_asymmetric(matrix1, matrix2):
-    matrix1[:], matrix2[:] = matrix2[:], matrix1[:]
+def zero(size):
+    return [[0 for _ in range(size)] for _ in range(size)]
 
 
-try:
-    K = int(input("Введите множитель К: "))
-    n = int(input("Введите N, больше 5 для создания квадратной матрицы: "))
+def inputm(mat, i1, i2, j1, j2):
+    zero_mat = zero(len(mat) // 2)
+    for i in range(i1, i2):
+        for j in range(j1, j2):
+            zero_mat[i - i1][j - j1] = mat[i][j]
+    return zero_mat
 
+
+K = int(input('Введите число K: '))
+n = int(input('Введите число число N, больше или равное 5: '))
+
+if n > 5:
+    pass
+else:
+    print('Введен неправильное N, попробуйте еще')
     while n < 5:
-        n = int(input("Число меньше 5. Введите число больше 5 "))
+        n = int(input('Введите число число N, больше или равное 5: '))
 
-    del_n = n // 2
-    fixik = del_n
-    if n % 2 != 0:
-        fixik += 1
+answer = input(
+    'Для использование единичной матрицы напишите 1, для использования случайно сгенерированной напишите 2: ')
+if answer not in ['1', '2']:
+    print('Попробуйте ещё')
+    while answer not in ['1', '2']:
+        n = int(input(
+            'Для использование единичной матрицы напишите 1, для использования случайно сгенерированной напишите 2: '))
+if answer == '1':
+    A = [[(1) for i in range(n)] for j in range(n)]
+elif answer == '2':
+    A = [[random.randint(-10, 10) for i in range(n)] for j in range(n)]
 
-    matrix_A = [[random.randint(-10, 10) for _ in range(n)] for _ in range(n)]
-    print("\nМатрица A:")
-    print_matrix(matrix_A)
+print('Матрица А:')
+printm(A)
 
-    matrix_F = [row.copy() for row in matrix_A]
-    print("\nМатрица F:")
-    print_matrix(matrix_F)
+half_n = n // 2
+fix_n = half_n
+if n % 2 != 0:
+    fix_n += 1
 
-    matrix_C = [row[fixik:] for row in matrix_F[:del_n]]
-    print('\nПодматрица C:')
-    print_matrix(matrix_C)
+B = inputm(A, 0, half_n, fix_n, n)
+C = inputm(A, fix_n, n, fix_n, n)
+D = inputm(A, fix_n, n, 0, half_n)
+E = inputm(A, 0, half_n, 0, half_n)
 
-    matrix_B = [row[:del_n] for row in matrix_F[:del_n]]
-    matrix_D = [row[:del_n] for row in matrix_F[del_n:]]
-    matrix_E = [row[fixik:] for row in matrix_F[fixik:]]
+print('Подматрицы матрицы A:')
+print('Подматрица B')
+printm(B)
+print('Подматрица C')
+printm(C)
+print('Подматрица D')
+printm(D)
+print('Подматрица E')
+printm(E)
 
-    sum_area2 = sum(matrix_C[0][1:del_n-1])
-    sum_area3 = sum(matrix_C[del_n-1][1:del_n-1])
+simp, summ = 0, 0
 
-    if sum_area3 > sum_area2:
-        print("В В количество чисел, меньших К в нечетных столбцах в области 3 больше.")
-        print("Симметрично меняем местами область 3 и 2 в Е.")
-        swap_symmetric(matrix_E, 1, 1)
-        print('\n Модифицированная подматрица E:')
-        print_matrix(matrix_E)
-    else:
-        print("Сумма чисел в области 3 меньше или равна сумме чисел в области 2.")
-        print("Меняем подматрицы В и Е асимметрично.")
-        swap_asymmetric(matrix_B, matrix_E)
+# область 2
+for i in range(n // 4, half_n):
+    for j in range(half_n - i - 1, i + 1):
+        if (i + 1) % 2 == 0:
+            simp += C[i][j]
 
-    matrix_F = matrix_A.copy()
-    for i in range(del_n):
-        for j in range(del_n):
-            matrix_F[i][j] = matrix_B[i][j]
-    for i in range(fixik, n):
-        for j in range(fixik, n):
-            matrix_F[i][j] = matrix_E[i - fixik][j - fixik]
+# область 3
+for i in range((n // 4) + 1):
+    for j in range(i, half_n - i):
+        if (j + 1) % 2 != 0:
+            if B[j][i] < K:
+                summ += B[j][i]
 
-    print('\nИзменённая матрица F:')
-    print_matrix(matrix_F)
+print('сумма чисел в нечетных столбцах в области 3: ', summ)
 
-    matrix_AF = [[sum(K * matrix_A[i][k] * matrix_F[k][j] for k in range(n)) for j in range(n)] for i in range(n)]
-    print('\nРезультат (K*A)*F:')
-    print_matrix(matrix_AF)
+print('сумма чисел в четных строках в области 2:', simp)
 
-    matrix_A_transp = [[matrix_A[j][i] for i in range(n)] for j in range(n)]
+if summ > simp:
+    print('поменяем в Е симметрично области 2 и 3 местами')
+    print('Начальная подматрциа Е:')
+    printm(E)
+    for i in range((n // 4) + 1):
+        for j in range(i, half_n - i):
+            E[j][i], E[j][((n // 4) + 1) - i] = E[j][((n // 4) + 1) - i], E[j][i]
+    print('Получившаяся подматрица С:')
+    printm(E)
+else:
+    print('E и В поменяем местами несимметрично')
+    E, B = B, E
 
-    matrix_KAT = [[K * matrix_A_transp[i][j] for j in range(n)] for i in range(n)]
-    print('\nРезультат K*A^T:')
-    print_matrix(matrix_KAT)
+F = A.copy()
+paste(F, D, 0, 0)
+paste(F, E, fix_n, 0)
+paste(F, B, fix_n, fix_n)
+paste(F, C, 0, fix_n)
 
-    matrix_res = [[matrix_AF[i][j] - matrix_KAT[i][j] for j in range(n)] for i in range(n)]
-    print("\nРезультат (K*A)*F - K*A^T:")
-    print_matrix(matrix_res)
+print('Матрица F:')
+printm(F)
 
-except ValueError:
-    print("Вводимое значение не является числом. Пожалуйста, введите число.")
+
+print('Вычисляем (К*A)*F – (K * AT):')
+
+
+At = zero(n)
+print("Матрица А транспонированая:")
+for i in range(n):
+    for j in range(n):
+        At[i][j] = A[j][i]
+printm(At)
+
+
+KA = A.copy()
+for i in range(n):
+    for j in range(n):
+        KA[i][j] *= K
+print('Результат K * A:')
+printm(KA)
+
+
+KAF = zero(n)
+for i in range(n):
+    for j in range(n):
+        KAF[i][j] = (KA[i][j] * F[i][j]) // 2
+print('Результат (К*A) * F:')
+printm(KAF)
+
+
+KAt = At.copy()
+for i in range(n):
+    for j in range(n):
+        KAt[i][j] *= K
+print('Результат K * AT:')
+printm(KAt)
+
+
+result = zero(n)
+for i in range(n):
+    for j in range(n):
+        result[i][j] = KAF[i][j] - KAt[i][j]
+print('Результат:')
+printm(result)
